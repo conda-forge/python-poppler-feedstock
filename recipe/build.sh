e3bea0f35a3417e548eeb55b2f0fede74f74f947
Bela Stoyan <bela.stoyan@gmail.com>
2023-10-11 13:24:17 +0200

    try setting cross-file manually

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,17 @@
 set +e
 set -x
-meson setup _build ${MESON_ARGS}
-meson compile -C _build
-meson install -C _build --no-rebuild
+
+echo $MESON_ARGS
+
+if [[ -f "$BUILD_PREFIX/meson_cross_file.txt" ]];
+then
+    cat $BUILD_PREFIX/meson_cross_file.txt
+
+    $PYTHON -m pip install . -vv --no-build-isolation \
+        --config-settings=setup-args=--cross-file=$BUILD_PREFIX/meson_cross_file.txt \
+        --config-settings builddir=_build
+else
+    $PYTHON -m pip install . -vv --no-build-isolation --config-settings builddir=_build
+fi
+
 cat _build/meson-logs/meson-log.txt
