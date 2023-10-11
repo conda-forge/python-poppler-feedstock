088715cf082a948c3ed50bd8435de8e3c230c0c2
Bela Stoyan <bela.stoyan@gmail.com>
2023-10-11 18:40:49 +0200

    use scipy setup

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,22 @@
-set +e
-set -x
+#!/bin/bash
+set -ex
 
-echo $MESON_ARGS
+mkdir builddir
 
-if [[ -f "$BUILD_PREFIX/meson_cross_file.txt" ]];
-then
-    echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
+# Taken from https://github.com/conda-forge/scipy-feedstock/blob/main/recipe/build.sh
 
-    cat $BUILD_PREFIX/meson_cross_file.txt
+# HACK: extend $CONDA_PREFIX/meson_cross_file that's created in
+# https://github.com/conda-forge/ctng-compiler-activation-feedstock/blob/main/recipe/activate-gcc.sh
+# https://github.com/conda-forge/clang-compiler-activation-feedstock/blob/main/recipe/activate-clang.sh
+# to use host python; requires that [binaries] section is last in meson_cross_file
+echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
 
-    $PYTHON -m pip install . -vv --no-build-isolation \
-        --config-settings=setup-args=--cross-file=$BUILD_PREFIX/meson_cross_file.txt \
-        --config-settings builddir=_build
-else
-    $PYTHON -m pip install . -vv --no-build-isolation --config-settings builddir=_build
-fi
+# meson-python already sets up a -Dbuildtype=release argument to meson, so
+# we need to strip --buildtype out of MESON_ARGS or fail due to redundancy
+MESON_ARGS_REDUCED="$(echo $MESON_ARGS | sed 's/--buildtype release //g')"
 
-cat _build/meson-logs/meson-log.txt
+# -wnx flags mean: --wheel --no-isolation --skip-dependency-check
+$PYTHON -m build -w -n -x \
+    -Cbuilddir=builddir \
+    -Csetup-args=${MESON_ARGS_REDUCED// / -Csetup-args=} \
+    || (cat builddir/meson-logs/meson-log.txt && exit 1)
