56d6ed3e14a322cefb4cce489f2b53d1fc233dac
Bela Stoyan <bela.stoyan@gmail.com>
2023-10-11 18:09:52 +0200

    use host python

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,8 @@ echo $MESON_ARGS
 
 if [[ -f "$BUILD_PREFIX/meson_cross_file.txt" ]];
 then
+    echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
+
     cat $BUILD_PREFIX/meson_cross_file.txt
 
     $PYTHON -m pip install . -vv --no-build-isolation \
